3ba5d0587b223dca85e37d41224ec05c0fcf5eed
jTommy786 <jose.julian.bonilla786@gmail.com>
2026-08-27 15:55:21 -0500

    Nested loops deleted to accomplish O(n), using dictionaries

--- 1_TwoSum/main.py
+++ 1_TwoSum/main.py
@@ -1,22 +1,12 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-
-        found = False
-
-        for i in range(len(nums)):
-            sum = 0
-            for j in range(len(nums)):
-                print(nums.index(nums[i]))
-                print(nums.index(nums[j]))
-                if i != j:
-                    sum = nums[i]+nums[j]
-                    print("In")
-                    if target == sum and found!=True:
-                        found=True
-                        positions = [i,j]
-
-                        print("Found in: " + str(positions))
-                        return positions
+        seen_nums={}
+        for index, value in enumerate(nums):
+            residue = target - value
+            if residue in seen_nums:
+                return seen_nums[residue], index
+            seen_nums[value] = index
+        return []
                     
                     
                 
