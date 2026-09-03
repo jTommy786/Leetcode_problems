b924cd59c7d3ecad199de92c8a398345065a6be6
jTommy786 <jose.julian.bonilla786@gmail.com>
2026-09-03 12:11:10 -0500

    main solution

---
+++ 13_Roman_Integer/main.py
@@ -0,0 +1,34 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        result = 0
+        for i in range(len(s)):
+            if s[i] == "I":
+                if s[i:i+2] == 'IV':
+                    result-=1
+                elif s[i:i+2] == 'IX':
+                    result-=1
+                else:
+                    result+=1
+            if s[i]=='V':
+                result+=5
+            if s[i]=='X':
+                if s[i:i+2] == 'XL':
+                    result-=10
+                elif s[i:i+2] == 'XC':
+                    result-=10
+                else:
+                    result+=10
+            if s[i]=='L':
+                result+=50
+            if s[i]=='C':
+                if s[i:i+2] == 'CD':
+                    result-=100
+                elif s[i:i+2] == 'CM':
+                    result-=100
+                else:
+                    result+=100
+            if s[i]=='D':
+                result+=500
+            if s[i]=='M':
+                result+=1000
+        return result
